be45eff1f58f78cfe6addd1889b243b30dc1e9fd
Luis Henrique <lhzamperlini@gmail.com>
2022-09-07 18:02:44 -0300

    Correções Esteticas

--- routes/TripRoutes.js
+++ routes/TripRoutes.js
@@ -3,9 +3,8 @@ const router = express.Router()
 const tripController = require('../controllers/TripController')
 const checkAuth = require('../helpers/auth').checkAuth
 
-
 router.get('/dashboard', checkAuth, tripController.dashboard)
 router.get('/', tripController.scheduleTrip)
 router.post('/', tripController.scheduleTripPost)
-
+router.get('/pendding', checkAuth, tripController)
 module.exports = router
