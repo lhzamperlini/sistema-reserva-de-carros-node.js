d461ed5354e81ebf65c162e05aae3602a24bbf12
Luis Henrique <lhzamperlini@gmail.com>
2022-09-10 11:26:33 -0300

    Aprovação de Viagens inserida e funcional

--- controllers/TripController.js
+++ controllers/TripController.js
@@ -1,5 +1,5 @@
 const pendingTrip = require('../models/PendingTrip')
-const approvedTrip = require('../models/ApprovedTrip')
+const aprovedTrip = require('../models/ApprovedTrip')
 
 module.exports = class TripController {
     static dashboard(req, res) {
@@ -40,6 +40,39 @@ module.exports = class TripController {
         pendingTrip.destroy({ where: { id: id } })
             .then(() => {
                 req.flash('message', 'A viagem foi reprovada com sucesso!')
+                res.render('trips/dashboard')
+            })
+            .catch((err) => console.log(err))
+    }
+
+    static aproveTrip(req, res) {
+        const id = req.params.id
+
+        pendingTrip.findOne({ where: { id: id }, raw: true })
+            .then((trip) => {
+                res.render('trips/toAprove', { trip })
+            })
+            .catch((err) => console.log())
+    }
+
+    static aproveTripPost(req, res) {
+        const trip = {
+            passenger: req.body.passenger,
+            email: req.body.email,
+            departurePlace: req.body.departure_place,
+            destiny: req.body.destiny,
+            travelData: req.body.data,
+            departureTime: req.body.departure_time,
+            returnTime: req.body.return_time,
+            driver: req.body.driver,
+            car: req.body.car
+        }
+
+        aprovedTrip.create(trip)
+            .then((trip) => {
+                // Aqui será inserida uma parte para enviar um email ao requerente confirmando a solicitação
+                // Só tenho que aprender como se faz isso.
+                req.flash('message', 'A viagem foi aprovada, um email de confirmação foi enviado ao requerente!')
                 res.render('trips/dashboard')
             })
             .catch((err) => console.log(err))
--- routes/TripRoutes.js
+++ routes/TripRoutes.js
@@ -8,4 +8,6 @@ router.get('/', tripController.scheduleTrip)
 router.post('/', tripController.scheduleTripPost)
 router.get('/pending', checkAuth, tripController.showPendingTrip)
 router.post('/reprove', checkAuth, tripController.reproveTrip)
+router.get('/aprove/:id', checkAuth, tripController.aproveTrip)
+router.post('/aprove', checkAuth, tripController.aproveTripPost)
 module.exports = router
